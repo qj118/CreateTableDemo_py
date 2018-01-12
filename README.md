aedf4657f8e85393c4cb8d78ebaa455f36889299
qj118 <qj1180405@163.com>
2018-01-12 15:51:00 +0800

    + excel function and modify README.md

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-Demo of Create Table in Files
------------------------------
+# Word and Excel 操作
 
-- genTable.py -- 利用python-docx，生成一个年度目标的表格 
+## 文件说明
+
+- genTable.py
+ + 利用python-docx，在word文档中生成一个年度目标的表格
+ + 利用openpyxl，在excel中生成一个年度的目标表格
+
+## 使用说明
+
+- 安装 python-docx `pip install python-docx`
+- 安装 openpyxl `pip install openpyxl`
+
+## 修改说明
+
+- 修改年份 `year = xxxx`
+- 修改列名
+ + Word 需要加一列
+ + Excel直接在`row0`里面加列名
+
+
+## 存在问题
+
+- Word中单元格文字无法居中
+- Excel 单元格无法自动调整大小 
--- genTable.py
+++ genTable.py
@@ -3,16 +3,15 @@ from docx.shared import Inches
 from docx.enum.style import WD_STYLE_TYPE
 from docx.enum.table import WD_TABLE_ALIGNMENT
 import datetime
+from openpyxl import Workbook
+from openpyxl.styles import Font, Color, Alignment
 
 year = 2018
 
-s = datetime.datetime(year, 1, 1)
-year_end = datetime.datetime(year, 12, 31)
-weeks = year_end.strftime("%W")
-
-delta = datetime.timedelta(days = 6 - s.weekday())
+# create word table
 
 document = Document()
+
 # styles = document.styles
 # for s in styles:
 # 	if s.type == WD_STYLE_TYPE.TABLE:
@@ -29,13 +28,17 @@ document = Document()
 
 #year_end = s + datetime.timedelta(days = year_long - 1)
 
+s = datetime.datetime(year, 1, 1)
+year_end = datetime.datetime(year, 12, 31)
+weeks = year_end.strftime("%W")
 
+delta = datetime.timedelta(days = 6 - s.weekday())
 
 table = document.add_table(rows = int(weeks), cols = 5, style = "Light Grid Accent 1")
 table.alignment = WD_TABLE_ALIGNMENT.CENTER
 
 heading_cells = table.rows[0].cells
-heading_cells[0].text = '2018'
+heading_cells[0].text = str(year)
 heading_cells[1].text = '书'
 heading_cells[2].text = '菜'
 heading_cells[3].text = '电影'
@@ -57,3 +60,38 @@ if (remain >= 5):
 
 document.add_page_break()
 document.save('demo.docx')
+
+# create xlsx table
+
+x_document = Workbook()
+sheet = x_document.create_sheet()
+sheet.title = "年度报表"
+
+sheet['A1'] = str(year)
+sheet['A1'].font = Font(bold = True, italic = True)
+row0 = ['书', '电影', '算法']
+b = Font(bold = True)
+a = Alignment(horizontal = "center", wrap_text = True)
+for i in range(2, len(row0) + 2):
+	sheet.cell(row = 1, column = i, value = row0[i-2])
+	sheet.cell(row = 1, column = i).font = b
+	sheet.cell(row = 1, column = i).alignment = a
+
+s = datetime.datetime(year, 1, 1)
+delta = datetime.timedelta(days = 6 - s.weekday())
+for i in range(2, int(weeks) + 1):
+	e = "(" + s.strftime("%m/%d") + '--' + (s + delta).strftime("%m/%d") + ")"
+	sheet.cell(row = i, column = 1, value = "第%d周\n" % (i - 1) + e)
+	sheet.cell(row = i, column = 1).font = b
+	sheet.cell(row = i, column = 1).alignment = a
+	s = s + datetime.timedelta(days = 7)
+	delta = datetime.timedelta(days = 6)
+
+remain = (year_end - (s - datetime.timedelta(days = 1))).days
+if (remain >= 5):
+	e = "(" + s.strftime("%m/%d") + '--' + year_end.strftime("%m/%d") + ")"
+	sheet.cell(row = 54, column = 1, value = "第53周\n" + e)
+	sheet.cell(row= 54, column = 1).font = b
+	sheet.cell(row = 54, column = 1).alignment = a
+
+x_document.save('demo.xlsx')
